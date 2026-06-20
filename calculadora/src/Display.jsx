ef4bab8abd5bdbff1124c0f30afa11c8e1a1aa0b
educarlettodeabreu <educarlettodeabreu@gmail.com>
2026-06-20 11:18:38 -0300

    componente display e teclado criados

---
+++ calculadora/src/Display.jsx
@@ -0,0 +1,9 @@
+function Display() {
+  return (
+    <div>
+      <p>0</p>
+    </div>
+  );
+}
+
+export default Display;
---
+++ calculadora/src/Teclado.jsx
@@ -0,0 +1,5 @@
+function Teclado() {
+  return <div></div>;
+}
+
+export default Teclado;
