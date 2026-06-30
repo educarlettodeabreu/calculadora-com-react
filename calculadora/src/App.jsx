21a988ea0bbd002810d0961dfaed06903bc139d6
educarlettodeabreu <educarlettodeabreu@gmail.com>
2026-06-30 18:31:16 -0300

    .

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Display from "./Display.jsx";
 import Teclado from "./Teclado.jsx";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { Calcularequacao } from "./Calcularequacao.js";
 import Historico from "./Historico.jsx";
 function App() {
@@ -17,28 +17,31 @@ function App() {
     setequacao(conta);
   };
 
-  const aoClicar = (valor) => {
-    if (valor === "C") {
-      setequacao("");
-    } else if (valor === "BACK") {
-      setequacao(equacao.slice(0, -1));
-    } else if (valor === "=") {
-      if (
-        /[+\-*/^]{2,}/.test(equacao) ||
-        /^[+*/^]/.test(equacao) ||
-        /[+\-*/^]$/.test(equacao)
-      ) {
-        setequacao("Equação inválida");
-        setTimeout(() => setequacao(""), 800);
-        return;
+  const aoClicar = useCallback(
+    (valor) => {
+      if (valor === "C") {
+        setequacao("");
+      } else if (valor === "BACK") {
+        setequacao(equacao.slice(0, -1));
+      } else if (valor === "=") {
+        if (
+          /[+\-*/^]{2,}/.test(equacao) ||
+          /^[+*/^]/.test(equacao) ||
+          /[+\-*/^]$/.test(equacao)
+        ) {
+          setequacao("Equação inválida");
+          setTimeout(() => setequacao(""), 800);
+          return;
+        }
+        const resultado = Calcularequacao(equacao);
+        sethistorico([...historico, { conta: equacao, res: resultado }]);
+        setequacao(resultado);
+      } else {
+        setequacao(equacao + valor);
       }
-      const resultado = Calcularequacao(equacao);
-      sethistorico([...historico, { conta: equacao, res: resultado }]);
-      setequacao(resultado);
-    } else {
-      setequacao(equacao + valor);
-    }
-  };
+    },
+    [equacao, historico],
+  );
 
   return (
     <div id="calculadora">
